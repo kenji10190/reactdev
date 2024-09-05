3ea715df1db2774006fd7fe54f12764c3bb339b5
kenji10190 <oinari13579@gmail.com>
2024-09-05 19:01:03 +0900

    add minimax function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { minimax } from "./minimax";
+import { calculateWinner } from "./calculateWinner";
 
 // Squareコンポーネント
 function Square({value, onSquareClick}){
@@ -6,44 +8,42 @@ function Square({value, onSquareClick}){
 }
 
 // Boardコンポーネント
-function Board({xIsNext, squares, onPlay}) {
+function Board({xIsNext, squares, onPlay, ai}) {
 
   function handleClick(i){
     if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    
-    if (xIsNext) nextSquares[i] = "X";
-    else nextSquares[i] = "O";
+    nextSquares[i] = "O";
 
     onPlay(nextSquares);
+    if (!nextSquares.includes(null)) return;
+    ai(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
 
-  if (winner){
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
+  if (winner) status = "勝者は " + winner + "です!";
+  else if (!squares.includes(null)) status = "引き分けです!";
+  else status = "あなたの番です!";
 
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )  
@@ -62,14 +62,21 @@ export default function Game(){
     setCurrentMove(nextHistory.length - 1);
   }
 
+  function AIMove(squares){
+    let xMove = minimax(squares, 0, true);
+    const xSquares = squares.slice();
+    xSquares[xMove] = "X";
+    handlePlay(xSquares);
+  }
+
   function jumpTo(nextMove){
     setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) description = "Go to move #" + move;
-    else description = "Go to game start";
+    if (move > 0) description = move + "手目に戻る";
+    else description = "最初に戻る";
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
@@ -80,30 +87,11 @@ export default function Game(){
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} ai={AIMove} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
     </div>
   )
-}
-
-function calculateWinner(squares){
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-
-  for (let i = 0; i < lines.length; i++){
-    let [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return squares[a];
-  }
-  return null;
 }
---
+++ src/minimax.js
@@ -0,0 +1,43 @@
+import { calculateWinner } from "./calculateWinner";
+
+const scores = {
+    "X" : 10,
+    "O" : -10,
+    "tie" : 0
+  }
+
+export function minimax(board, depth, isMaximizing){
+  let haveWinner = calculateWinner(board);
+  if (haveWinner) return scores[haveWinner];
+  else if (!board.includes(null)) return scores["tie"];
+
+  let bestMove = null;
+  let bestScore = isMaximizing ? -Infinity : Infinity;
+
+  if (isMaximizing){
+    for (let i = 0; i < board.length; i++){
+      if (board[i] === null){
+        board[i] = "X";
+        let score = minimax(board, depth + 1, false);
+        board[i] = null;
+        if (score > bestScore){
+          bestScore = score;
+          if (depth === 0) bestMove = i;
+        }
+      }
+    }
+    if (depth === 0 && bestMove !== null) return bestMove;
+
+  } else {
+    for (let i = 0; i < board.length; i++){
+      if (board[i] == null){
+        board[i] = "O";
+        let score = minimax(board, depth + 1, true);
+        board[i] = null;
+        bestScore = Math.min(score, bestScore);
+      }
+    }
+  }
+
+  return bestScore;
+}
